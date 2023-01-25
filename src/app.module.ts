1839d2ddd632d205eac638eb8b802083b0599c0e
Vladimir Kuban <74879032+williamdampier@users.noreply.github.com>
2023-01-25 23:01:25 +1300

    foundation

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+// import { ValidationPipe } from './pipes/validation.pipe';
+async function start() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('MyShift App')
+    .setDescription('Backend documentation')
+    .setVersion('1.0.0')
+    .addTag('NestJS')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+  // app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT, () => {
+    console.log(`App is runnning on port: ${PORT}`);
+  });
+}
+
+start();
